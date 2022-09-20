6e5344ec5ea5a957cb10c85abfb217e73578c55d
Lodjic <loic.thiriet@protonmail.com>
2022-09-20 15:26:35 +0200

    Ajout fct calcul d'heuristique

--- player.py
+++ player.py
@@ -69,23 +69,72 @@ class PlayerControllerMinimax(PlayerController):
         random_move = random.randrange(5)
         return ACTION_TO_STR[random_move]
 
-    def minimax(self, node, player):
-        
-        curr_state = node.state
-        # if all fishes have been caught :
-        if curr_state.player_scores[0] + curr_state.player_scores[1] == 25:
-            return curr_state.player_scores[0] - curr_state.player_scores[1]  # end of the game we have the real utility function
 
+# Calculus fcts
+
+def minimax(node, player):
+    
+    curr_state = node.state
+    # if all fishes have been caught :
+    if curr_state.player_scores[0] + curr_state.player_scores[1] == 25:
+        return curr_state.player_scores[0] - curr_state.player_scores[1]  # end of the game we have the real utility function
+
+    else:
+        if player == 0:
+            v = - np.inf
+            children = node.compute_and_get_children()
+            for child in children :
+                v = max(v, minimax(child, 1))
+            return v
         else:
-            if player == 0:
-                v = - np.inf
-                children = node.compute_and_get_children()
-                for child in children :
-                    v = max(v, minimax(self, child, 1))
-                return v
-            else:
-                v = np.inf
-                children = node.compute_and_get_children()
-                for child in children :
-                    v = min(v, minimax(self, child, 0))
-                return v
+            v = np.inf
+            children = node.compute_and_get_children()
+            for child in children :
+                v = min(v, minimax(child, 0))
+            return v
+
+def heuristic(node):
+    """
+    Calcultate the heuristic function for a player at a given state
+    """
+    curr_state = node.state
+    curr_score = curr_state.player_scores[0] - curr_state.player_scores[1]
+    total_points_collected = curr_state.player_scores[0] + curr_state.player_scores[1]
+    # if all fishes have been caught :
+    if total_points_collected == 25:
+        return curr_score  # end of the game we have the real utility function
+    else:
+        closer_player = compute_closer_player_to_fish(curr_state)
+        # compute number of points to be won by each player
+
+def norm2_distance(position_array, p):
+    """
+    Calculate the euclidian norm between 2 points in 2D
+    """
+    return abs((position_array[1]-p[1])/(position_array[0]-p[0]))
+
+def norm2_distance_for_all_fishes(state, player):
+    """
+    Calculate the distance for all remaining fishes to the position of the player
+    """
+    fish_positions = [[p[0], p[1]] for p in list(state.fish_positions.values())]
+    player_position = state.hook_positions[player]
+    if player_position < 9:
+        mask = fish_positions[:, 0] > 10 + player_position
+        fish_positions[:, 0][mask] = -fish_positions[:, 0][mask]
+    elif player_position > 10:
+        mask = fish_positions[:, 0] < player_position - 10
+        fish_positions[:, 0][mask] = -fish_positions[:, 0][mask]
+    return norm2_distance(fish_positions, player_position)
+
+def compute_closer_player_to_fish(state):
+    distance_to_player_0 = norm2_distance_for_all_fishes(state, 0)
+    distance_to_player_1 = norm2_distance_for_all_fishes(state, 0)
+    closer_player = [0]*len(distance_to_player_0)
+    
+    for i in range(len(distance_to_player_0)):
+        if distance_to_player_0[i] <= distance_to_player_1[i]:
+            closer_player[i] = 1
+    
+    return closer_player
+
