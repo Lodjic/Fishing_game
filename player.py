ec99600f9c24dedfa99b7f4ee476950ec85da4e0
Lodjic <loic.thiriet@protonmail.com>
2022-09-21 10:56:33 +0200

    Correction erreur et algo qui fonctionne

--- player.py
+++ player.py
@@ -65,19 +65,24 @@ class PlayerControllerMinimax(PlayerController):
 
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
+        children = initial_tree_node.compute_and_get_children()
+        values = [0] * len(children)
+        for i, child in enumerate(children):
+            values[i] = minimax(child, 1, max_depth=2)
+        index = values.index(max(values))  # Careful might have several move with same value
 
-        random_move = random.randrange(5)
-        return ACTION_TO_STR[random_move]
+        return ACTION_TO_STR[children[index].move]
 
 
 # Calculus fcts
 
-def minimax(node, player):
+def minimax(node, player, max_depth=5):
     
     curr_state = node.state
+    remaining_points = sum(list(curr_state.fish_scores.values()))
     # if all fishes have been caught :
-    if curr_state.player_scores[0] + curr_state.player_scores[1] == 25:
-        return curr_state.player_scores[0] - curr_state.player_scores[1]  # end of the game we have the real utility function
+    if remaining_points == 0 or node.depth >= max_depth:
+        return heuristic(node)  # end of the game (real utility function) or max_depth reached (approxiamtion through heuristic)
 
     else:
         if player == 0:
@@ -100,38 +105,43 @@ def heuristic(node):
     curr_state = node.state
     curr_score = curr_state.player_scores[0] - curr_state.player_scores[1]
     total_points_collected = curr_state.player_scores[0] + curr_state.player_scores[1]
+    remaining_points = sum(list(curr_state.fish_scores.values()))
     # if all fishes have been caught :
-    if total_points_collected == 25:
+    if remaining_points == 0:  # NOT REALLY THE RIGHT CONDITION : TBM !!!
         return curr_score  # end of the game we have the real utility function
     else:
-        closer_player = compute_closer_player_to_fish(curr_state)
-        # compute number of points to be won by each player
+        remaining_fishes = list(curr_state.fish_positions.keys())
+        fish_scores = np.array([curr_state.fish_scores[i] for i in remaining_fishes])
+        closer_player = np.array(compute_closer_player_to_fish(curr_state))
+        mask0 = closer_player == 0
+        return curr_score + sum(fish_scores[mask0]) - sum(fish_scores[~mask0])
+        
 
 def norm2_distance(position_array, p):
     """
     Calculate the euclidian norm between 2 points in 2D
     """
-    return abs((position_array[1]-p[1])/(position_array[0]-p[0]))
+    return np.sqrt((position_array[:, 1]-p[1]) ** 2 + (position_array[:, 0]-p[0]) ** 2)
 
 def norm2_distance_for_all_fishes(state, player):
     """
     Calculate the distance for all remaining fishes to the position of the player
     """
-    fish_positions = [[p[0], p[1]] for p in list(state.fish_positions.values())]
+    fish_positions = np.array([[p[0], p[1]] for p in list(state.fish_positions.values())])
     player_position = state.hook_positions[player]
-    if player_position < 9:
-        mask = fish_positions[:, 0] > 10 + player_position
+    if player_position[0] < 9:
+        mask = fish_positions[:, 0] > (10 + player_position[0])
         fish_positions[:, 0][mask] = -fish_positions[:, 0][mask]
-    elif player_position > 10:
-        mask = fish_positions[:, 0] < player_position - 10
+    elif player_position[0] > 10:
+        mask = fish_positions[:, 0] < (player_position[0] - 10)
         fish_positions[:, 0][mask] = -fish_positions[:, 0][mask]
     return norm2_distance(fish_positions, player_position)
 
 def compute_closer_player_to_fish(state):
     distance_to_player_0 = norm2_distance_for_all_fishes(state, 0)
-    distance_to_player_1 = norm2_distance_for_all_fishes(state, 0)
-    closer_player = [0]*len(distance_to_player_0)
+    distance_to_player_1 = norm2_distance_for_all_fishes(state, 1)
     
+    closer_player = [0]*len(distance_to_player_0)
     for i in range(len(distance_to_player_0)):
         if distance_to_player_0[i] <= distance_to_player_1[i]:
             closer_player[i] = 1
