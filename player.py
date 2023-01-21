b010c126bfe53b13531f843608c8149e6f151976
Lodjic <loic.thiriet@protonmail.com>
2023-01-21 17:41:14 +0100

    Commenting and cleaning

--- player.py
+++ player.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import numpy as np
 import time
 import random
@@ -54,7 +53,7 @@ class PlayerControllerMinimax(PlayerController):
 
     def search_best_next_move(self, initial_tree_node):
         """
-        Use minimax (and extensions) to find best possible next move for player 0 (green boat)
+        Function that initiate the minimax algo on the root node to find best possible next move for player 0 (green boat)
         :param initial_tree_node: Initial game tree node
         :type initial_tree_node: game_tree.Node
             (see the Node class in game_tree.py for more information!)
@@ -62,10 +61,6 @@ class PlayerControllerMinimax(PlayerController):
         :rtype: str
         """
 
-        # EDIT THIS METHOD TO RETURN BEST NEXT POSSIBLE MODE USING MINIMAX ###
-
-        # NOTE: Don't forget to initialize the children of the current node
-        #       with its compute_and_get_children() method!
         t0 = time.time()
         depth_max = 0
         index = 0
@@ -96,14 +91,22 @@ class PlayerControllerMinimax(PlayerController):
         return ACTION_TO_STR[action]
 
 
-# Calculus fcts
+
+############################################## CALCULUS FUNCTIONS ##############################################
+
 def minimax(t0, hash_table, node, player, alpha, beta, max_depth=5):
+    """
+    Function that compute the minimax algo on a certain node in order to find best possible next move for player 0 (green boat).
+    It is a recursive function that initiate the search at next depth (ie for the children nodes) if there is still enough time.
+    In fact we only have 75e-3 seconds to choose an action.
+    """
     curr_state = node.state
     remaining_fishes = len(list(curr_state.fish_positions.keys()))
     # If close to timeout we stop the search :
     if time.time() - t0 > 0.055:
         return -np.inf
-    # if all fishes have been caught :
+    # If all fishes have been caught, it is not necesary to go deeper because there is no more action to optimize. So we return the heuristic.
+    # Also if we are at the max depth the current node is a terminal leaf so we also return the heuristic
     elif remaining_fishes == 0 or node.depth >= max_depth:
         # To avoid repeated stateswe check if we did not already calculate the value
         hash_code_state = compute_hash_code(node)
@@ -115,6 +118,7 @@ def minimax(t0, hash_table, node, player, alpha, beta, max_depth=5):
             hash_table[hash_code_state] = value
             return value  # terminal leaf of the tree because end of the game (real utility function) or max_depth reached (approxiamtion through heuristic)
 
+    # In all other cases we compute the children of the current node and recursively call the minimax fct while using alpha-beta pruning.
     else: 
         children = node.compute_and_get_children()
 
@@ -130,7 +134,7 @@ def minimax(t0, hash_table, node, player, alpha, beta, max_depth=5):
                 v = max(v, minimax(t0, hash_table, child, 1, alpha, beta, max_depth))
                 alpha = max(alpha, v)
                 if beta <= alpha:
-                    break
+                    break  # alpha-beta pruning
         else:
             v = np.inf
             children = reorder(children, argsort(children_score))
@@ -138,23 +142,31 @@ def minimax(t0, hash_table, node, player, alpha, beta, max_depth=5):
                 v = min(v, minimax(t0, hash_table, child, 0, alpha, beta, max_depth))
                 beta = min(beta, v)
                 if beta <= alpha:
-                    break
+                    break  # alpha-beta pruning
         return v
 
 def heuristic(node):
     """
     Calcultate the heuristic function for a player at a given state
     """
+    # Compute the current score (positive if player 0 leads, negative in the other case)
     curr_state = node.state
     curr_score = curr_state.player_scores[0] - curr_state.player_scores[1]
     
+    # If no more fishes to catch, returns the current score
     if len(list(curr_state.fish_positions.keys())) == 0:
         return curr_score
     
+    # Gets to know if MAX and MIN have caught any fish
     fish_caught_by_MAX = curr_state.player_caught[0]
     fish_caught_by_MIN = curr_state.player_caught[1]
 
-    closests_fishes = get_closest_fish_for_loop(curr_state)
+    # Find the closest fish to MAX and MIN with their respective distance to the player in question
+    closests_fishes = get_closest_fish(curr_state)
+
+    # If one of the player gets a fish, we add its score (positively for MAX, negatively for MIN) to the game score. 
+    # If not, we add half of the closest fish score (positively for MAX, negatively for MIN) normalized by its distance ot the player
+    # (19+10) is the max distance between a hook and a fish in this game if we take into account that the adversary boat can block MAX.
     if fish_caught_by_MAX != -1:
         curr_score += curr_state.fish_scores[fish_caught_by_MAX]
     else:
@@ -166,7 +178,7 @@ def heuristic(node):
     
     return curr_score
 
-def norm_distance_for_loop(position_list, p):
+def norm_distance(position_list, p):
     """
     Calculate the norm between 2 points in 2D
     """
@@ -176,6 +188,11 @@ def norm_distance_for_loop(position_list, p):
     return distance
 
 def get_fish_positions(state):
+    """
+    Return 2 lists : the list of fishes id and the corresponding list of fishes positions
+    But it returns those list only for the fishes with positives scores
+    If there are no more fishes with positive scores we return those list for all the remaining fishes
+    """
     fish_scores = state.fish_scores
     fish_positions = state.fish_positions
     positions_list = []
@@ -189,9 +206,9 @@ def get_fish_positions(state):
     else:
         return list(state.fish_positions.keys()), [[p[0], p[1]] for p in list(state.fish_positions.values())]
 
-def norm_distance_for_all_fishes_for_loop(state, player):
+def norm_distance_for_all_fishes(state, player):
     """
-    Calculate the distance for all remaining fishes to the position of the player
+    Calculate the distance between a fish and the player for all the remaining fishes
     """
     fish_real_indexes, fish_positions = get_fish_positions(state)
     player_position = state.hook_positions[player]
@@ -204,11 +221,14 @@ def norm_distance_for_all_fishes_for_loop(state, player):
         for i in range(len(fish_positions)):
             if fish_positions[i][0] <= opponent_position[0]:
                 fish_positions[i][0] += 20
-    return fish_real_indexes, norm_distance_for_loop(fish_positions, player_position)
+    return fish_real_indexes, norm_distance(fish_positions, player_position)
 
-def get_closest_fish_for_loop(state):
-    fish_real_indexes_MAX, distance_to_MAX = norm_distance_for_all_fishes_for_loop(state, 0)
-    fish_real_indexes_MIN, distance_to_MIN = norm_distance_for_all_fishes_for_loop(state, 1)
+def get_closest_fish(state):
+    """
+    Returns the closest fish to MAX and to MIN with their respective corresponding distance to MAX and to MIN
+    """
+    fish_real_indexes_MAX, distance_to_MAX = norm_distance_for_all_fishes(state, 0)
+    fish_real_indexes_MIN, distance_to_MIN = norm_distance_for_all_fishes(state, 1)
     dist_min_to_MAX = min(distance_to_MAX) # we take the first one if several fishes are equidistant
     dist_min_to_MIN = min(distance_to_MIN) # we take the first one if several fishes are equidistant
     score_fishes = [state.fish_scores[i] for i in fish_real_indexes_MAX]
@@ -216,18 +236,27 @@ def get_closest_fish_for_loop(state):
     return ((ind_fish_score_min, dist_min_to_MAX), (ind_fish_score_min, dist_min_to_MIN))
 
 def compute_hash_code(node):
+    """
+    Computes the hash of a node thanks to the different positions of the players and teh fishes
+    """
     hash_code = str(node.state.hook_positions[0][0]) + str(node.state.hook_positions[0][1]) + str(node.state.hook_positions[1][0]) + str(node.state.hook_positions[1][1])
     for i in list(node.state.fish_positions.keys()):
         hash_code += str(node.state.fish_positions[i][0]) + str(node.state.fish_positions[i][1])
     return hash_code
 
 def argsort(seq):
+    """
+    numpy.argsort on a list
+    """
     return sorted(range(len(seq)), key=seq.__getitem__)
 
 def reorder(list, order_seq):
     return [list[i] for i in order_seq]
 
 def where_equal(l, condition):
+    """
+    Finds the indexes at which a list l equals a condition
+    """
     indexes = []
     for i in range(len(l)):
         if l[i] == condition:
